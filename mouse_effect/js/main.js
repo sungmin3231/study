309782b6b96cc6ed4f0e33bdaf766ba758ffdc8a
sungmin3231 <psungmin88.1@gmail.com>
2020-12-07 16:28:26 +0900

    mousemove effect 좌표값구하기

---
+++ mouse_effect/js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const vertical = document.querySelector(".vertical");
+const horizontal = document.querySelector(".horizontal");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
+
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  console.log(`${x}, ${y}`);
+
+  vertical.style.left = `${x}px`;
+  horizontal.style.top = `${y}px`;
+  target.style.left = `${x}px`;
+  target.style.top = `${y}px`;
+  tag.style.top = `${y}px`;
+  tag.style.left = `${x}px`;
+  tag.innerHTML = `
+    좌표 (${x}px, ${y}px)
+  `;
+});
