47acb12f6f58de5df09a4f9f893e0e863011494c
sungmin3231 <psungmin88.1@gmail.com>
2020-12-12 03:01:50 +0900

    delegation으로 이벤트 위임해서 shopping_list 개선

--- shopping_list/js/main.js
+++ shopping_list/js/main.js
@@ -24,32 +24,25 @@ function onAdd() {
   input.focus();
 }
 
+let id = 0; // UUID, 오브젝트 해시코드로 고유한 아이디를 쓰는게 좋음.
 function createItem(text) {
+  // delegation
+
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
-
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const name = document.createElement("span");
-  name.setAttribute("class", "item__name");
-  name.innerText = text;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item__divider");
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute("data-id", id);
+  itemRow.innerHTML = `
+      <li class="item__row">
+        <div class="item">
+          <span class="item__name">${text}</span>
+            <button class="item__delete">
+              <i class="fas fa-trash-alt" data-id=${id}></i>
+            </button>
+        </div>
+        <div class="item__divider"></div>
+      </li>
+  `;
+  id++;
   return itemRow;
 }
 addBtn.addEventListener("click", () => {
@@ -59,5 +52,13 @@ addBtn.addEventListener("click", () => {
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     onAdd();
+  }
+});
+
+items.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
