ce617ed14abf01438d96932e3137165034f5c3ce
sungmin3231 <psungmin88.1@gmail.com>
2021-01-12 15:13:06 +0900

    habit 컴포넌트 만들기

--- React/react-basic/habit-tracker/src/app.jsx
+++ React/react-basic/habit-tracker/src/app.jsx
@@ -1,7 +1,9 @@
 import "./app.css";
+import Habit from "./components/habit";
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const name = "sungmin";
+  return <Habit />;
 }
 
 export default App;
