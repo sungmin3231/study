77fe6c6c3ce3c77dea11542ad9be7de4cb3bb80a
sungmin3231 <psungmin88.1@gmail.com>
2021-01-12 15:29:15 +0900

    state이해하기 (state라는 오브젝트를 통해서(setState) 데이터를 업데이트가 발생하면 리액트가 자동적으로 render함수를 호출함)

--- React/react-basic/habit-tracker/src/components/habit.jsx
+++ React/react-basic/habit-tracker/src/components/habit.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
+  state = {
+    count: 0,
+  };
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
   render() {
     return (
       <li className="habit">
         <span className="habit-name">Reading</span>
-        <span className="habit-count">8</span>
-        <button className="habit-button habit-increase">
+        <span className="habit-count">{this.state.count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-button habit-decrease">
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
           <i className="fas fa-minus-square"></i>
         </button>
         <button className="habit-button habit-delete">
