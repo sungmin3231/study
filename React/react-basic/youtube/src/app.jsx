4282717f5629a9b0ebf9b0fa0770672ce95e38a9
sungmin3231 <psungmin88.1@gmail.com>
2021-01-13 21:37:46 +0900

    add youtube service

--- React/react-basic/youtube/src/app.jsx
+++ React/react-basic/youtube/src/app.jsx
@@ -3,44 +3,23 @@ import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 import styles from "./app.module.css";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDZ7h-0EaA4-GAZS2LaNZWmlZp-7_O_9IY`,
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result =>
-        result.items.map(item => ({ ...item, id: item.id.videoId }))
-      )
-      .then(items => setVideos(items))
-      .catch(error => console.log("error", error));
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDZ7h-0EaA4-GAZS2LaNZWmlZp-7_O_9IY",
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
   }, []);
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />;
+      <VideoList videos={videos} />
     </div>
   );
 }
