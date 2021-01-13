96aba246e96b3dd51f7e4d8bfbb5f9ed12bf7d0c
sungmin3231 <psungmin88.1@gmail.com>
2021-01-13 21:11:41 +0900

    검색기능 구현

--- React/react-basic/youtube/src/app.jsx
+++ React/react-basic/youtube/src/app.jsx
@@ -1,10 +1,27 @@
 import { useEffect, useState } from "react";
-import "./app.css";
-
+import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
+import styles from "./app.module.css";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDZ7h-0EaA4-GAZS2LaNZWmlZp-7_O_9IY`,
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result =>
+        result.items.map(item => ({ ...item, id: item.id.videoId }))
+      )
+      .then(items => setVideos(items))
+      .catch(error => console.log("error", error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -20,7 +37,12 @@ function App() {
       .then(result => setVideos(result.items))
       .catch(error => console.log("error", error));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />;
+    </div>
+  );
 }
 
 export default App;
