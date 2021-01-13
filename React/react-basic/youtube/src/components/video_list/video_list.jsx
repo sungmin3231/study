5d800012a0bb259dd8c1a1a5c8d8fa4fc2b76c51
sungmin3231 <psungmin88.1@gmail.com>
2021-01-13 20:23:47 +0900

    make video item

--- React/react-basic/youtube/src/components/video_list/video_list.jsx
+++ React/react-basic/youtube/src/components/video_list/video_list.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import VideoItem from "../video_item/video_item";
+import styles from "./video_list.module.css";
 
 const VideoList = props => (
-  <ul>
+  <ul className={styles.videos}>
     {props.videos.map(video => (
-      <VideoItem ket={video.id} video={video} />
+      <VideoItem key={video.id} video={video} />
     ))}
   </ul>
 );
